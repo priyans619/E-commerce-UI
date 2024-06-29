f419c559b3aa54de18f704770210e8550e9f1252
priyans619 <priyansh.2b@gmail.com>
2024-06-29 22:37:39 +0530

    add Footer comp

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 p-4">
+      <div className="container mx-auto text-center text-white">
+        &copy; 2024 E-Commerce. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
