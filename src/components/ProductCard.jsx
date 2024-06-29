677bec8f250abb3792182f02ea960effcb0fc0ab
priyans619 <priyansh.2b@gmail.com>
2024-06-29 23:50:57 +0530

    Add to cart functionality

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { addToCart } from '../redux/cartSlice';
 
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
+
+  const handleAddToCart = () => {
+    dispatch(addToCart(product));
+  };
 
   return (
     <div className="border p-4 rounded-lg shadow-lg w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto flex flex-col bg-gray-200" style={{ background: 'linear-gradient( #DDEEED 100.17%, #FDF1E0 94.92%)' }}>
@@ -12,6 +17,7 @@ const ProductCard = ({ product }) => {
         <p className="text-gray-700 mb-2">${product.price}</p>
         <p className="text-yellow-400 mb-2">Ratings {product.rating}/5</p>
       </div>
+      <button onClick={handleAddToCart} className="bg-gray-500 text-gray-500 p-2 rounded mt-4" style={{ background: 'linear-gradient(239.26deg, #DDEEED 0.17%, #FDF1E0 0.92%)' }}>Add to Cart</button>
     </div>
   );
 };
