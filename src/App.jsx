209228ed9042845c96cf369bb13a09a528cc0e2c
priyans619 <priyansh.2b@gmail.com>
2024-06-29 23:44:40 +0530

    Update App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import Products from './pages/Products';
+import Cart from './pages/Cart';
 
 const App = () => {
   return (
@@ -10,7 +11,12 @@ const App = () => {
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow container mx-auto px-4">
-          
+          <Routes>
+            <Route exact path="/" element={<Products />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            {/* <Route path="/account" element={<Account />} /> */}
+          </Routes>
         </main>
         <Footer />
       </div>
