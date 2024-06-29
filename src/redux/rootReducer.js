be737ace733bb0791b81f556c94f2243f060b5fb
priyans619 <priyansh.2b@gmail.com>
2024-06-29 23:05:46 +0530

    add product and cart slice

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,11 @@
 
 import { combineReducers } from 'redux';
+import productReducer from './productSlice';
+import cartReducer from './cartSlice';
 
 const rootReducer = combineReducers({
-  
+  products: productReducer,
+  cart: cartReducer,
 });
 
 export default rootReducer;
