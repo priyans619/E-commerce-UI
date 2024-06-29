19623387fadaa2d572264d3cef5d954958777e88
priyans619 <priyansh.2b@gmail.com>
2024-06-29 22:38:23 +0530

    Link to respective route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,12 @@ const Header = () => {
     <header className="bg-gray-900 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-white text-xl font-bold">MyIndia</Link>
-        
+        <nav>
+          <Link to="/products" className="text-white mx-3">Home</Link>
+          <Link to="/products" className="text-white mx-3">Products</Link>
+          <Link to="/cart" className="text-white mx-3">Cart</Link>
+          <Link to="/account" className="text-white mx-3">Account</Link>
+        </nav>
       </div>
     </header>
   );
