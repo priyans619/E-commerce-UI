3eaad61c9424da0e2637b93a5455f9ee3fa537ac
priyans619 <priyansh.2b@gmail.com>
2024-06-29 23:11:16 +0530

    add card comp

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProductCard = ({ }) => {
+
+return (
+    <div className="border p-4 rounded-lg shadow-lg w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto flex flex-col bg-gray-200" style={{ background: 'linear-gradient( #DDEEED 100.17%, #FDF1E0 94.92%)' }}>
+      
+    </div>
+  );
+};
+
+export default ProductCard;
+
