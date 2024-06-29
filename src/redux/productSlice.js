ce89fd371ec1d09db2e85389b766aab69caf90bb
priyans619 <priyansh.2b@gmail.com>
2024-06-29 23:20:28 +0530

    name,initialState,reducers for product

--- src/redux/productSlice.js
+++ src/redux/productSlice.js
@@ -2,7 +2,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const productSlice = createSlice({
- 
+  name: 'products',
+  initialState: [],
+  reducers: {
+    setProducts: (state, action) => action.payload,
+  },
 });
 
 export const { setProducts } = productSlice.actions;
+export default productSlice.reducer;
