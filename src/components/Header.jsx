119c5f5011a5a21d8a6b74acede58a3c43f7f63e
priyans619 <priyansh.2b@gmail.com>
2024-06-30 14:30:44 +0530

    final UI header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,13 @@ const Header = () => {
     <header className="bg-gray-300 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/">
-          <img src={logo} alt="MyIndia" className="h-8 sm:h-10 md:h-10 lg:h-12" /> 
+          <img src={logo} alt="MyIndia" className="h-2 sm:h-8 md:h-10 lg:h-12" /> 
         </Link>
-        <nav>
-          <Link to="/products" className="text-white mx-5">Home</Link>
-          <Link to="/products" className="text-white mx-5">Products</Link>
-          <Link to="/cart" className="text-white mx-5">Cart</Link>
-          <Link to="/account" className="text-white mx-5">Account</Link>
+        <nav className='text-xl text-black'>
+          <Link to="/products" className=" mx-5">Home</Link>
+          <Link to="/products" className=" mx-5">Products</Link>
+          <Link to="/cart" className=" mx-5">Cart</Link>
+          <Link to="/account" className="">Account</Link>
         </nav>
       </div>
     </header>
