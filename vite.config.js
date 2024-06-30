87047f4e06555b6de9bd6529274e0fda8d60eb9a
priyans619 <priyansh.2b@gmail.com>
2024-06-30 19:36:41 +0530

    Add plugins for PWA render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        short_name: 'MyIndia',
+        name: 'MyIndia E-Commerce Platform',
+        icons: [
+          
+        ],
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#ffffff'
+      }
+    })
+  ]
+});
