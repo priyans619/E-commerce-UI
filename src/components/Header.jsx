6d8c8fd67b6a351eec7ee746a39eaaf992f5264c
priyans619 <priyansh.2b@gmail.com>
2024-06-30 19:34:59 +0530

    add home route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
-    <header className="bg-gray-300 p-4">
+    <header className="bg-gray-300 p-4 ">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/">
-          <img src={logo} alt="MyIndia" className="h-2 sm:h-8 md:h-10 lg:h-12" /> 
+        <img src={logo} alt="MyIndia" className="h-7 sm:h-8 md:h-10 lg:h-12" /> 
         </Link>
         <nav className='text-xl text-black sm:mx-3 md:mx-4 lg:mx-5'>
-          <Link to="/products" className=" mx-5">Home</Link>
+          <Link to="/" className=" mx-5">Home</Link>
           <Link to="/products" className=" mx-5">Products</Link>
           <Link to="/cart" className=" mx-5">Cart</Link>
           <Link to="/account" className="">Account</Link>
