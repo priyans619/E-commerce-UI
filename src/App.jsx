0b1b0ebf7f188a8492a62d1f5d3228a3377174ae
priyans619 <priyansh.2b@gmail.com>
2024-06-30 19:38:34 +0530

    render Home page and bg

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 import Products from './pages/Products';
 import Cart from './pages/Cart';
 
@@ -10,9 +11,9 @@ const App = () => {
     <Router>
       <div className="flex flex-col min-h-screen">
         <Header />
-        <main className="flex-grow container mx-auto px-4">
+        <main className="flex-grow container mx-auto px-4 bg-gray-300" >
           <Routes>
-            <Route exact path="/" element={<Products />} />
+            <Route exact path="/" element={<Home />} />
             <Route path="/products" element={<Products />} />
             <Route path="/cart" element={<Cart />} />
             {/* <Route path="/account" element={<Account />} /> */}
