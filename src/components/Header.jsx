940314cdb2e77a9d73601b39b7d5ac84c20a4d0f
priyans619 <priyansh.2b@gmail.com>
2024-06-30 13:55:50 +0530

    logo with with all screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,20 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
-    <header className="bg-gray-900 p-4">
+    <header className="bg-gray-300 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-white text-xl font-bold">MyIndia</Link>
+        <Link to="/">
+          <img src={logo} alt="MyIndia" className="h-8 sm:h-10 md:h-10 lg:h-12" /> 
+        </Link>
         <nav>
-          <Link to="/products" className="text-white mx-3">Home</Link>
-          <Link to="/products" className="text-white mx-3">Products</Link>
-          <Link to="/cart" className="text-white mx-3">Cart</Link>
-          <Link to="/account" className="text-white mx-3">Account</Link>
+          <Link to="/products" className="text-white mx-5">Home</Link>
+          <Link to="/products" className="text-white mx-5">Products</Link>
+          <Link to="/cart" className="text-white mx-5">Cart</Link>
+          <Link to="/account" className="text-white mx-5">Account</Link>
         </nav>
       </div>
     </header>
